9f383beb3142aa31aa56b4fc8f9a0ea520eb8f49
tanveerahmad1517 <tanveerobjects@gmail.com>
2018-05-21 20:17:16 +0500

    sdferer

--- myblogproject/settings.py
+++ myblogproject/settings.py
@@ -134,9 +134,9 @@ LOGIN_REDIRECT_URL = 'posts:all'
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-      os.path.join(BASE_DIR, "static"),
-    ]
-STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
-MEDIA_URL = '/media/'
+# STATICFILES_DIRS = [
+#       os.path.join(BASE_DIR, "static"),
+#     ]
+# STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
+# MEDIA_URL = '/media/'
 django_heroku.settings(locals())
