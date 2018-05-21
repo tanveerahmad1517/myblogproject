cc96b00934697a39a2a2732a48c2ee837ebc4fb7
tanveerahmad1517 <tanveerobjects@gmail.com>
2018-05-21 21:02:10 +0500

    wwwwwwwwwddds

--- myblogproject/settings.py
+++ myblogproject/settings.py
@@ -136,8 +136,10 @@ LOGIN_REDIRECT_URL = 'posts:all'
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [
-      os.path.join(BASE_DIR, "static"),
-    ]
-STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
-MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
